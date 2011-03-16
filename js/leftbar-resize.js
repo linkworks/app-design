1c82038ebbaa8572c5978177f76eae12ca791b51
Ian Murray <ianmurrays@gmail.com>
2011-03-16 16:22:00 -0300

    Adding jQuery UI, moving css and js to own files

---
+++ js/leftbar-resize.js
@@ -0,0 +1,28 @@
+$(function(){
+  /* Left bar resizing */
+  (function(){
+    var started_dragging = false,
+        start_x;
+    
+    $('#leftbar_bottom .resizer').mousedown(function(e){
+      e.preventDefault();
+      started_dragging = true;
+      start_x = e.pageX;
+    });
+    
+    $('*').mouseup(function(e){
+      e.preventDefault();
+      started_dragging = false;
+    });
+    
+    $('*').mousemove(function(e){
+      e.preventDefault();
+      if (started_dragging && e.pageX >= 190) {
+        var newX = e.pageX - start_x;
+        start_x = e.pageX;
+        $('#leftbar').css('width', $('#leftbar').width() + newX);
+        $('#leftbar_bottom').css('width', $('#leftbar_bottom').width() + newX);
+      }
+    });
+  })();
+});
